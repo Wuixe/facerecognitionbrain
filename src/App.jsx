4f3ded18e82cb46c7bdccf946d4528d780233f99
Wuixe <guillaume.renel@gmail.com>
2023-02-27 22:06:08 +0100

    version control + clarifai starting to work

--- src/App.jsx
+++ src/App.jsx
@@ -4,89 +4,9 @@ import Logo from './components/Logo/Logo.jsx';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.jsx';
 import Rank from './components/Rank/Rank.jsx';
 import { useCallback } from "react";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
+import ParticlesBg from 'particles-bg'
 import FaceRecognition from './components/FaceRecognition/FaceRecognition.jsx';
 import './App.css';
-import Clarifai from "clarifai";
-
-const app = new Clarifai.App({
-    apiKey: ''
-   });
-
-// Section that I can't put anywhere else about the settings of my backgroud particles
-const particleOptions =  {
-  background: {
-      color: {
-          value: 'linear-gradient(89deg, #FF5EDF 0%, #04C8DE 100%)',
-      },
-  },
-  fpsLimit: 120,
-  interactivity: {
-      events: {
-          onClick: {
-              enable: true,
-              mode: "push",
-          },
-          onHover: {
-              enable: true,
-              mode: "repulse",
-          },
-          resize: true,
-      },
-      modes: {
-          push: {
-              quantity: 4,
-          },
-          repulse: {
-              distance: 200,
-              duration: 0.4,
-          },
-      },
-  },
-  particles: {
-      color: {
-          value: "#ffffff",
-      },
-      links: {
-          color: "#ffffff",
-          distance: 150,
-          enable: true,
-          opacity: 0.5,
-          width: 1,
-      },
-      collisions: {
-          enable: true,
-      },
-      move: {
-          directions: "none",
-          enable: true,
-          outModes: {
-              default: "bounce",
-          },
-          random: false,
-          speed: 2,
-          straight: false,
-      },
-      number: {
-          density: {
-              enable: true,
-              area: 800,
-          },
-          value: 50,
-      },
-      opacity: {
-          value: 0.5,
-      },
-      shape: {
-          type: "circle",
-      },
-      size: {
-          value: { min: 1, max: 5 },
-      },
-  },
-  detectRetina: true,
-}
 
 
 
@@ -96,19 +16,16 @@ function App() {
     const [input, setInput] = useState('');
     const [imageURL, setImageURL] = useState('');
 
-    //particle functions
-    const particlesInit = useCallback(async engine => {
-    console.log(engine);
-    await loadFull(engine);
-    }, []);
 
-    const particlesLoaded = useCallback(async container => {
-        await console.log(container);
-      }, []);
+    const PAT = 'd3ef2d5c8d1c43daa605c92d9154056e';
+    const USER_ID = 'clarifai';       
+    const APP_ID = 'main';
+    const MODEL_ID = 'face-detection';    
+    const IMAGE_URL = 'https://images.pexels.com/photos/2422290/pexels-photo-2422290.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
+
 
     
-    
-    //Event functions 
+
 
     const onInputChange = (e) => {
         setInput(e.target.value);
@@ -117,26 +34,43 @@ function App() {
     const onSubmit = () => {
         setImageURL(input);
         console.log('click');
+
+        const raw = JSON.stringify({
+            "user_app_id": {
+                "user_id": USER_ID,
+                "app_id": APP_ID
+            },
+            "inputs": [
+                {
+                    "data": {
+                        "image": {
+                            "url": IMAGE_URL
+                        }
+                    }
+                }
+            ]
+        });
+    
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Key ' + PAT
+            },
+            body: raw
+        };
         
-        // app.models
-        // .predict(
-        //   {
-        //     id: 'face-detection',
-        //     name: 'face-detection',
-        //     version: '6dc7e46bc9124c5c8824be4822abe105',
-        //     type: 'visual-detector',
-        //   }, input)
-        // .then(response => {
-        //   console.log('hi', response)
-        // })
-        // .catch(err => console.log(err));
+        fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
+        .then(response => response)
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
     }
 
 
 
       return (
     <div className="App">
-      <Particles className='particles' id="tsparticles" init={particlesInit} loaded={particlesLoaded} options={particleOptions}/>
+      <ParticlesBg color="#FFFFFF" num={50} type="cobweb" bg={true} />
       <Navigation />
       <Logo />
       <Rank/>
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,4 +1,4 @@
-import Tilt from 'react-tilt';
+import Tilt from 'react-parallax-tilt';
 import './Logo.css';
 import brain from './brain.png';
 
